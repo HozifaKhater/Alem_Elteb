95bcb6880c4947d6bd928b0a3ec99edd27392845
Hozifa Khater <hozifa.khater@hotmail.com>
2020-04-27 20:59:25 +0200

    Add project files.

---
+++ DAL/Context/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Context
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        private readonly IConfiguration _config; 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IConfiguration config) : base(options)
+        {
+            _config = config;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
